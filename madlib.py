1d970402beee6c489d0409dc278365210d4d07ee
Mark Rattle <111889289+energeist@users.noreply.github.com>
2022-08-28 12:14:40 -0400

    Added madlib.py
    Simple madlib generator.

---
+++ madlib.py
@@ -0,0 +1,86 @@
+
+# Homework: Create a madlib. Imagine a story where some of the words are 
+# supplied by user input. Using python you will use input to collect 
+# words for a story and then display the story. 
+
+# Use input to collect each word to a variable
+# Use an f string to display the story
+
+# Your madlib must collect at least 6 words!
+
+print("MADLIB GENERATOR\n")
+name = input("Please enter a name: ")
+p_t_verb = input("Please enter a verb in past tense: ")
+adj1 = input("Please enter an adjective: ")
+thing = input("Please enter the name of an object: ")
+adj2 = input("Please enter second adjective: ")
+activity = input("Please enter your favourite activity: ")
+print("")
+print(f"{name} {p_t_verb} when they received their {adj1} {thing} for winning first place in the {adj2} {activity} competition!\n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# --------------------------------------------------
+# Partial solution
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# name = input("Name:")
+# color = input("color:")
+# num = input("Number:")
+
+# print(f"{name} wore {color} shoes while they counted to {num}")
